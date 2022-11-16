16ed199d8e7f07ae597b0f31c5956366c82bfe77
Mohammad Maheer <mohammad.maheer@smaconsulting.ca>
2022-11-16 11:14:05 -0700

    cleaned Game.js

--- src/Game.js
+++ src/Game.js
@@ -17,35 +17,6 @@ export class Game {
     const dx = toX - x;
     const dy = toY - y;
 
-    console.log("canMoveKnight is called");
-
-    /*let validAPI = "/api/project/moveValidity/" + dx + "/" + dy;
-    if (process.env.NODE_ENV !== "production") {
-      validAPI =
-        "http://localhost:7229/api/project/moveValidity/" + dx + "/" + dy;
-    }
-    fetch(validAPI, {
-      method: "GET",
-      headers: {*/
-    //Accept: "application/json, text/plain, */*",
-    /*"Content-Type": "application/json",
-      },
-    }).then((response) => {
-      if (!response.ok) {
-        response.text().then((text) => {
-          throw Error(response.statusText + ": " + text);
-        });
-      } else {
-        response.json().then((result) => {
-          console.log("\n" + "result.isValid: " + result.isValid + "\n");
-          //setTimeout(() => {
-            return result.isValid;
-          }, 1000);
-          return result.isValid;
-        });
-      }
-    });*/
-
     return (
       (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
       (Math.abs(dx) === 1 && Math.abs(dy) === 2)
